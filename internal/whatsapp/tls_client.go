f75d6416bda3a3f09fe786b2e1886985b6d726fb
Fahad Bin Hussain <fahadbinhussain001@gmail.com>
2026-06-19 16:05:34 +0600

    fix: simplify TLS client to use Chrome-like cipher suites

--- internal/whatsapp/tls_client.go
+++ internal/whatsapp/tls_client.go
@@ -1,34 +1,36 @@
 package whatsapp
 
 import (
-	"context"
 	"crypto/tls"
 	"net"
 	"net/http"
 	"time"
-
-	utls "github.com/refraction-networking/utls"
 )
 
 func NewChromeHTTPClient(proxyAddr string) *http.Client {
 	transport := &http.Transport{
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: false,
+			NextProtos:         []string{"h2", "http/1.1"},
+			CipherSuites: []uint16{
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			},
 		},
-		DialTLSContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-			// Create uTLS connection that impersonates Chrome
-			utlsConn, err := utls.Dial("tcp", addr, &utls.Config{
-				InsecureSkipVerify: false,
-				NextProtos:         []string{"h2", "http/1.1"},
-			})
-			if err != nil {
-				return nil, err
-			}
-			return utlsConn, nil
-		},
+		DialContext: (&net.Dialer{
+			Timeout:   15 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
 		MaxIdleConns:          100,
 		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:  10 * time.Second,
+		TLSHandshakeTimeout:  15 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 	}
 
